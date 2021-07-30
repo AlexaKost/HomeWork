10cb3aeed036f4dbaf9b92da7e6f978b4216c918
Alexandra Kostyuchenko <all2115@mail.ru>
2021-07-31 01:12:20 +0500

    add new branch lesson02

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const money = 1; 
-let income = 2; 
-const addExpenses = 3; 
-let deposit = 4; 
-const mission = 5;
-let period = 6;
+let money = 100000; 
+console.log(typeof money);
 
-alert('Доброго времени суток!');
-console.log('Хорошего и позитивного настроения!');
-console.log(money);
+let income = "Фриланс"; 
+console.log(typeof income);
+
+const addExpenses = 'Интернет, такси, коммунальные расходы'; 
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' ,'));
+
+const deposit = true;
+console.log(typeof deposit);
+
+const mission = 1000000;
+console.log( 'Цель заработать ' + mission + ' рублей' );
+
+const period = 6;
+console.log( 'Период равен ' + period + ' месяцев' );
+
+let budgetDay = money;
+budgetDay /= 30;
+console.log(budgetDay);
+
+
+
+
