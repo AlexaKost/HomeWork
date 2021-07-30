3291aea1660f9093927f0405447fedc243697457
Alexandra Kostyuchenko <all2115@mail.ru>
2021-07-30 13:15:13 +0500

    Deleted fail README.md

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+alert('Доброго времени суток!');
+console.log('Хорошего и позитивного настроения!');
+
+
+
 const money = 1; 
 let income = 2; 
 const addExpenses = 3; 
 let deposit = 4;
 const mission = 5;
 let period = 6;
-
-alert('Доброго времени суток!');
-console.log('Хорошего и позитивного настроения!');
 
